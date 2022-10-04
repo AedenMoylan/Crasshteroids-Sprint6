6f0a8c03718e5d757cbfbaf69178cc6d918a097e
AedenMoylan <aedenmoylan@gmail.com>
2022-10-04 17:49:39 +0100

    Laser destroys asteroid completed CR-3

---
+++ Assets/RW/Tests/TestScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class TestScript
+{
+    private Game game;
+
+    [SetUp]
+
+    public void Setup()
+    {
+        GameObject gameGameObject =
+        MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Game"));
+        game = gameGameObject.GetComponent<Game>();
+    }
+
+    [TearDown]
+    public void Teardown()
+    {
+        Object.Destroy(game.gameObject);
+    }
+
+    [UnityTest]
+    public IEnumerator LaserDestroysAsteroid()
+    {
+        // 1
+        GameObject asteroid = game.GetSpawner().SpawnAsteroid();
+        asteroid.transform.position = Vector3.zero;
+        GameObject laser = game.GetShip().SpawnLaser();
+        laser.transform.position = Vector3.zero;
+        yield return new WaitForSeconds(0.1f);
+        // 2
+        UnityEngine.Assertions.Assert.IsNull(asteroid);
+    }
+}
